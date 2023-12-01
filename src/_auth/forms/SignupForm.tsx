25557af3ebc761ef7d95f4b4d922200a40d37112
mckaymichael <mckaymichaelk@gmail.com>
2023-12-01 00:08:41 -0800

    working on the sign up

--- src/_auth/forms/SignupForm.tsx
+++ src/_auth/forms/SignupForm.tsx
@@ -1,5 +1,5 @@
 import { zodResolver } from "@hookform/resolvers/zod";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 import { useToast } from "@/components/ui/use-toast"
 import { Button } from "@/components/ui/button";
@@ -17,13 +17,16 @@ import { SignupValidation } from "@/lib/validation";
 import { z } from "zod";
 import Loader from "@/components/ui/shared/Loader";
 import { useCreateUserAccount, useSignInAccount } from "@/lib/react-query/queriesAndMutations";
+import { useUserContext } from "@/context/AuthContext";
 
 const SignupForm = () => {
   const { toast } = useToast()
+  const { checkAuthUser, isLoading: isUserLoading } = useUserContext();
+  const navigate = useNavigate();
 
-  const { mutateAsync: createUserAccount, isLoading: isCreatingUser } = useCreateUserAccount();
+  const { mutateAsync: createUserAccount, isPending: isCreatingAccount } = useCreateUserAccount();
 
-  const {mutateAsync: signInAccount, isLoading: isSignIn } = useSignInAccount;
+  const { mutateAsync: signInAccount, isPending: isSigningIn } = useSignInAccount();
 
   // 1. Define your form.
   const form = useForm<z.infer<typeof SignupValidation>>({
@@ -55,7 +58,14 @@ const SignupForm = () => {
       return toast({ title: 'Sign in failed. Please try again.'})
     }
 
-    
+    const isLoggedIn = await checkAuthUser();
+
+    if(isLoggedIn) {
+      form.reset();
+      navigate('/')
+    } else {
+      return toast({ title: 'Sign up failed. Please try again.' })
+    }
   }
 
   return (
@@ -122,7 +132,7 @@ const SignupForm = () => {
             )}
           />
           <Button type="submit" className="shad-button_primary">
-            {isCreatingUser ? (
+            {isCreatingAccount ? (
               <div className="flex-center gap-2">
                 <Loader />Loading...
               </div>
--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -1,5 +1,7 @@
+import { getCurrentUser } from '@/lib/appwrite/api';
 import { IContextType, IUser } from '@/types';
 import { createContext, useContext, useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 export const INITIAL_USER = {
     id: '',
@@ -26,9 +28,27 @@ const AuthProvider = ({ children }: { children: React.ReactNode }) => {
     const [isLoading, setIsLoading] = useState(false);
     const [isAuthenticated, setIsAuthenticated] = useState(false);
 
+    const navigate = useNavigate();
+
     const checkAuthUser = async () => {
         try {
             const currentAccount = await getCurrentUser();
+
+            if(currentAccount) {
+                setUser({
+                    id: currentAccount.$id,
+                    name: currentAccount.name,
+                    username: currentAccount.username,
+                    email: currentAccount.email,
+                    imageUrl: currentAccount.imageUrl,
+                    bio: currentAccount.bio,
+                })
+
+                setIsAuthenticated(true);
+
+                return true;
+            }
+            return false;
         } catch (error) {
             console.log(error);
             return false;
@@ -36,6 +56,15 @@ const AuthProvider = ({ children }: { children: React.ReactNode }) => {
             setIsLoading(false);
         }
     };
+
+    useEffect(() => {
+        if(
+            localStorage.getItem('cookieFallback') === '[]' || 
+            localStorage.getItem('cookieFallback') === null
+        ) navigate('/sign-in')
+
+        checkAuthUser();
+    }, []);
 
     const value = {
         user,
@@ -53,4 +82,6 @@ const AuthProvider = ({ children }: { children: React.ReactNode }) => {
     )
 }
 
-export default AuthContext
+export default AuthProvider;
+
+export const useUserContext = () => useContext(AuthContext);
