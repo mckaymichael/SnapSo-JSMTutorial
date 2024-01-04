a1227f9cce71dd50a71db76cf518ab6da8cc8814
mckaymichael <mckaymichaelk@gmail.com>
2024-01-03 16:10:55 -0800

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -29,7 +29,7 @@ const App = () => {
           <Route path="/posts/:id" element={<PostDetails />}/>
           <Route path="/profile/:id/*" element={<Profile />}/>
           <Route path="/update-profile/:id" element={<UpdateProfile />}/>
-          {/* <Route path="/liked-posts" element={<LikedPosts />}/> */}
+          <Route path="/liked-posts" element={<LikedPosts />}/>
         </Route>
       </Routes>
       <Toaster />
